755a66c7352965f0e0eb03d2ff8dcbda5f56ec64
kgni <admin@mdia.dk>
2022-07-02 12:08:11 +0200

    add mongoose, dotenv and create mongo file that will add a document to the database

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+	console.log('Connected to DB');
+	const PORT = process.env.PORT;
+	app.listen(PORT, () => {
+		console.log(`Server running on port ${PORT}`);
+	});
+});
+// models:
+const Note = require('./models/note');
 
 app.use(express.json());
 app.use(cors());
@@ -31,44 +43,38 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/notes', (request, response) => {
-	response.json(notes);
+	Note.find({}).then((notes) => {
+		response.json(notes);
+	});
 });
 
 app.get('/api/notes/:id', (request, response) => {
-	const id = Number(request.params.id);
-	const note = notes.find((note) => note.id === id);
-
-	if (note) {
+	Note.findById(request.params.id).then((note) => {
 		response.json(note);
-	} else {
-		response.status(404).end();
-	}
+	});
 });
 
-const generateId = () => {
-	const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-	return maxId + 1;
-};
+// const generateId = () => {
+// 	const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+// 	return maxId + 1;
+// };
 
 app.post('/api/notes', (request, response) => {
 	const body = request.body;
 
-	if (!body.content) {
-		return response.status(400).json({
-			error: 'content missing',
-		});
+	if (body.content === undefined) {
+		return response.status(400).json({ error: 'content missing' });
 	}
 
-	const note = {
+	const note = new Note({
 		content: body.content,
 		important: body.important || false,
 		date: new Date(),
-		id: generateId(),
-	};
+	});
 
-	notes = notes.concat(note);
-
-	response.json(note);
+	note.save().then((savedNote) => {
+		response.json(savedNote);
+	});
 });
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -76,9 +82,4 @@ app.delete('/api/notes/:id', (request, response) => {
 	notes = notes.filter((note) => note.id !== id);
 
 	response.status(204).end();
-});
-
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
 });
