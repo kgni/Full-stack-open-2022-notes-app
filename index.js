5878193260ced37718b05ab4b2a89e62efc9fbee
kgni <admin@mdia.dk>
2022-07-02 21:52:29 +0200

    update delete route to work with mongoose, and put route to update the important field, and error handling middleware for handling ObjectId.IsValid()

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 // models:
 const Note = require('./models/note');
 
+app.use(express.static('build'));
 app.use(express.json());
 app.use(cors());
-app.use(express.static('build'));
 
 let notes = [
 	{
@@ -48,10 +48,16 @@ app.get('/api/notes', (request, response) => {
 	});
 });
 
-app.get('/api/notes/:id', (request, response) => {
-	Note.findById(request.params.id).then((note) => {
-		response.json(note);
-	});
+app.get('/api/notes/:id', (request, response, next) => {
+	Note.findById(request.params.id)
+		.then((note) => {
+			if (note) {
+				response.json(note);
+			} else {
+				response.status(404).end();
+			}
+		})
+		.catch((error) => next(error));
 });
 
 // const generateId = () => {
@@ -77,9 +83,38 @@ app.post('/api/notes', (request, response) => {
 	});
 });
 
-app.delete('/api/notes/:id', (request, response) => {
-	const id = Number(request.params.id);
-	notes = notes.filter((note) => note.id !== id);
-
-	response.status(204).end();
+app.delete('/api/notes/:id', (request, response, next) => {
+	Note.findByIdAndRemove(request.params.id)
+		.then((result) => {
+			response.status(204).end();
+		})
+		.catch((error) => next(error));
 });
+
+app.put('/api/notes/:id', (request, response, next) => {
+	const body = request.body;
+
+	const note = {
+		content: body.content,
+		important: body.important,
+	};
+
+	Note.findByIdAndUpdate(request.params.id, note, { new: true })
+		.then((updatedNote) => {
+			response.json(updatedNote);
+		})
+		.catch((error) => next(error));
+});
+
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message);
+
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' });
+	}
+
+	next(error);
+};
+
+// handler of requests with result to errors
+app.use(errorHandler);
