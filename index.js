a2171f400789961aacc0e3b96b04930131773b50
kgni <admin@mdia.dk>
2022-07-04 11:21:54 +0200

    modify folder structure, separating routes and controllers into own module, creating utils folder for logging, config and middleware, refactor index.js

--- index.js
+++ index.js
@@ -1,129 +1,31 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const cors = require('cors')
-require('dotenv').config()
+const config = require('./utils/config');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const notesRouter = require('./controllers/notes');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
-	console.log('Connected to DB')
-	const PORT = process.env.PORT
-	app.listen(PORT, () => {
-		console.log(`Server running on port ${PORT}`)
+logger.info('connecting to', config.MONGO_URI);
+
+mongoose
+	.connect(config.MONGO_URI)
+	.then(() => {
+		logger.info('connected to MongoDB');
 	})
-})
-// models:
-const Note = require('./models/note')
+	.catch((error) => {
+		logger.error('error connecting to MongoDB:', error.message);
+	});
 
-app.use(express.static('build'))
-app.use(express.json())
-app.use(cors())
+app.use(cors());
+app.use(express.static('build'));
+app.use(express.json());
+app.use(middleware.requestLogger);
 
-let notes = [
-	{
-		id: 1,
-		content: 'HTML is easy',
-		date: '2022-05-30T17:30:31.098Z',
-		important: true,
-	},
-	{
-		id: 2,
-		content: 'Browser can execute only Javascript',
-		date: '2022-05-30T18:39:34.091Z',
-		important: false,
-	},
-	{
-		id: 3,
-		content: 'GET and POST are the most important methods of HTTP protocol',
-		date: '2022-05-30T19:20:14.298Z',
-		important: true,
-	},
-]
-app.get('/', (request, response) => {
-	response.send('<h1>Hello World!</h1>')
-})
+app.use('/api/notes', notesRouter);
 
-app.get('/api/notes', (request, response) => {
-	Note.find({}).then((notes) => {
-		response.json(notes)
-	})
-})
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
-app.get('/api/notes/:id', (request, response, next) => {
-	Note.findById(request.params.id)
-		.then((note) => {
-			if (note) {
-				response.json(note)
-			} else {
-				response.status(404).end()
-			}
-		})
-		.catch((error) => next(error))
-})
-
-// const generateId = () => {
-// 	const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-// 	return maxId + 1;
-// };
-
-app.post('/api/notes', (request, response, next) => {
-	const body = request.body
-
-	if (body.content === undefined) {
-		return response.status(400).json({ error: 'content missing' })
-	}
-
-	const note = new Note({
-		content: body.content,
-		important: body.important || false,
-		date: new Date(),
-	})
-
-	note
-		.save()
-		.then((savedNote) => {
-			response.json(savedNote)
-		})
-		.catch((error) => next(error))
-})
-
-app.delete('/api/notes/:id', (request, response, next) => {
-	Note.findByIdAndRemove(request.params.id)
-		.then((result) => {
-			response.status(204).end()
-		})
-		.catch((error) => next(error))
-})
-
-app.put('/api/notes/:id', (request, response, next) => {
-	const body = request.body
-
-	const note = {
-		content: body.content,
-		important: body.important,
-	}
-
-	Note.findByIdAndUpdate(request.params.id, note, {
-		new: true,
-		runValidators: true,
-	})
-		.then((updatedNote) => {
-			response.json(updatedNote)
-		})
-		.catch((error) => next(error))
-})
-
-const errorHandler = (error, request, response, next) => {
-	console.error(error.message)
-
-	if (error.name === 'CastError') {
-		return response.status(400).send({ error: 'malformatted id' })
-	}
-	if (error.name === 'ValidationError') {
-		return response.status(400).json({ error: error.message })
-	}
-
-	next(error)
-}
-
-// handler of requests with result to errors
-app.use(errorHandler)
+module.exports = app;
